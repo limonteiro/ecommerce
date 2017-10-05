98b7293dade455a9f004ef44e36aa81d72dbed97
Lilian Fukuda <lilian_fukuda@hotmail.com>
2017-10-05 13:55:36 -0300

    Lendo dados

--- index.php
+++ index.php
@@ -8,7 +8,13 @@ $app->config('debug', true);
 
 $app->get('/', function() {
     
-	echo "OK";
+	$sql = new Hcode\DB\Sql();
+
+	//echo "Ok";
+
+	$results = $sql->select("SELECT * FROM tb_users");
+
+	echo json_encode($results);
 
 });
 
